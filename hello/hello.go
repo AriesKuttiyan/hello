756e60657910477cdd7c2811e0fdbca58e79a4dc
AriesKuttiyan <aries.kuttiyan@outlook.com>
2017-01-02 14:16:18 +0530

    Add files via upload

--- hello/hello.go
+++ hello/hello.go
@@ -6,8 +6,9 @@ import (
 )
 
 func main() {
+    const x  = "Hello worlds\n"
     fmt.Printf("hello, world\n")
+    fmt.Printf(x)
     fmt.Printf(stringutil.Reverse("!oG ,olleH"))
     fmt.Printf("\n")
 }
-

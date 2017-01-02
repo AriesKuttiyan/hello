066286261424b98476ad511049cf41f05c860c84
AriesKuttiyan <aries.kuttiyan@outlook.com>
2017-01-02 13:24:23 +0530

    Add files via upload
    test

---
+++ hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+    "github.com/AriesKuttiyan/stringutil"
+)
+
+func main() {
+    fmt.Printf("hello, world\n")
+    fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+    fmt.Printf("\n")
+}
+

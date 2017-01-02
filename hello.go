73bf0a4e75ccfd2e62d01a201455687f1ec1e28f
AriesKuttiyan <aries.kuttiyan@outlook.com>
2017-01-02 14:09:50 +0530

    test-2

---
+++ hello.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "github.com/AriesKuttiyan/stringutil"
+)
+
+func main() {
+    const x  = "Hello worlds\n"
+    fmt.Printf("hello, world\n")
+    fmt.Printf(x)
+    fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+    fmt.Printf("\n")
+}
